afaf7858863f0dd2a5c5d70db53d7505b2d2154b
lifei <591955434@qq.com>
2019-08-22 21:22:29 +0800

    把本地工程推送git

---
+++ git-project/src/cn/lifei/FristTest.java
@@ -0,0 +1,16 @@
+package cn.lifei;
+
+public class FristTest {
+    public static void main(String[] args) {
+        System.out.println("8.22");
+        System.out.println("老婆");
+        System.out.println("我");
+        System.out.println("爱");
+        System.out.println("");
+    }
+
+    public void Test(){
+        int i=10;
+        i +=10;
+    }
+}
