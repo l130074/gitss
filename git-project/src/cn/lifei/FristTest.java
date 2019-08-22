b9ff169654254d720a3691a2b7759d7519936006
lifei <591955434@qq.com>
2019-08-22 21:43:35 +0800

    修改i的值

--- git-project/src/cn/lifei/FristTest.java
+++ git-project/src/cn/lifei/FristTest.java
@@ -6,11 +6,12 @@ public class FristTest {
         System.out.println("老婆");
         System.out.println("我");
         System.out.println("爱");
-        System.out.println("");
+        System.out.println("you");
     }
-
+//ok haha
     public void Test(){
         int i=0;
         i +=10;
+        System.out.println(i);
     }
 }
